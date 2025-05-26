9a5b8da0c61d4d0998720abbb782fe667b104c6c
ChuckStrout <charles.strout@reading.k12.ma.us>
2025-05-26 17:31:54 -0400

    Add files via upload

--- 2025 Grad Web Test.py
+++ 2025 Grad Web Test.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 # Load the Excel file
-df = pd.read_excel("2025SMEUD.xlsx")
+df = pd.read_csv("2025RGD.csv")
 
 # Display the DataFrame in Streamlit
-st.title("2025SMEUD.xlsx Viewer")
+st.title("2025 RGD Viewer")
 st.dataframe(df)
