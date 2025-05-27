d28486d99e8f064b24e0affda49178db269af17f
ChuckStrout <charles.strout@reading.k12.ma.us>
2025-05-26 21:14:38 -0400

    Add files via upload

--- 2025 Grad Web Test.py
+++ 2025 Grad Web Test.py
@@ -9,5 +9,25 @@ df = pd.read_csv(DATA_FILENAME)
 
 
 # Display the DataFrame in Streamlit
-st.title("2025 RGD Viewer")
-st.dataframe(df)
+st.title("2025 RMHS")
+st.title("Graduation Seating System")
+venue = st.radio("Select Venue", ("In PAC", "On Turf"))
+df["Full Name"] = df["Last Name"].astype(str) + ", " + df["First Name"].astype(str)
+selected_name = st.selectbox(
+    "Select Your Name",
+    options=sorted(df["Full Name"].unique()),
+    key="full_name_select"
+)
+
+if venue == "In PAC":
+    selected_row = df[df["Full Name"] == selected_name].iloc[0]
+    st.subheader("Starting in the PAC ")
+    st.text_input("Sit in the Section:", value=selected_row["SECTION"], disabled=True)
+    st.text_input("Sit in Row:", value=selected_row["PROW"], disabled=True)
+    st.text_input("Sit in Seat Number:", value=selected_row["PSEAT NUMBER"], disabled=True)
+if venue =="On Turf":
+    selected_row = df[df["Full Name"] == selected_name].iloc[0]
+    st.subheader("On The Turf")
+    st.text_input("You will be On the Side:", value=selected_row["SIDE"], disabled=True)
+    st.text_input("Sit in Row:", value=selected_row["ROW"], disabled=True)
+    st.text_input("Sit in Seat Number:", value=selected_row["SEAT NUMBER"], disabled=True)
